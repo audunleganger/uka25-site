3060d9bd0766b716caef3a50f0684ec3f05f0431
Audun Leganger <audun.leganger@gmail.com>
2024-06-11 18:22:57 +0200

    Add basic burger menu sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
-import "./Header.css"
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import "./Header.css"
+import Sidebar from "./Sidebar";
 
 export default function Header() {
+const [sidebarOpen, setSidebarOpen] = useState(false);
+
+const handleBurgerClick = () => {
+  setSidebarOpen(!sidebarOpen);
+}
+
   return (
     <div className="header-wrapper">
-        <Link to="/">
-            <img src="src/assets/uka-blindern-2017.png" alt="uka-logo" className="uka-logo" />
-        </Link>
-            <span className="header-button">Hjem</span>
-            <span className="header-button">Artister</span>
-            <span className="header-button">Program</span>
-            <span className="header-button">Om oss</span>
+      <div className="burger-wrapper">
+        <FiMenu className="burger" onClick={handleBurgerClick} />
+      </div>
+        <Sidebar className={sidebarOpen ? "sidebar sidebar-enabled" : "sidebar sidebar-disabled"} />
     </div>
   )
 }
