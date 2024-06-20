399c5f9b803825bf127d0d866d930c1f2ffdd7f2
Audun Leganger <audun.leganger@gmail.com>
2024-06-20 13:43:04 +0200

    WIP: Add cards to program field

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -1,1 +1,26 @@
-export default function ProgramCard() {}
+import "./ProgramCard.css";
+
+interface ProgramCardProps {
+    title: string;
+    date: string;
+    imagePath: string;
+    children: React.ReactNode;
+}
+
+export default function ProgramCard({
+    title,
+    date,
+    imagePath,
+    children,
+}: ProgramCardProps) {
+    return (
+        <div
+            className="program-card"
+            style={{ backgroundImage: `url(${imagePath})` }}
+        >
+            <h1 className="program-card-date">{date}</h1>
+            <h1 className="program-card-title">{title}</h1>
+            <p className="program-card-text">{children}</p>
+        </div>
+    );
+}
--- src/components/ProgramField.tsx
+++ src/components/ProgramField.tsx
@@ -1,11 +1,12 @@
 import "./ProgramCard";
+import ProgramCard from "./ProgramCard";
 import "./ProgramField.css";
 
 export default function ProgramField() {
     return (
         <section className="program-wrapper">
             <h1 className="program-header">Program: MiniUKA på Blindern</h1>
-            <div className="program-card-wrapper">
+            {/*<div className="program-card-wrapper">
                 <div className="program-entry">
                     <h3>Mandag 19. august</h3>
                     <p>Pub til pub og Battle of the Bands</p>
@@ -20,6 +21,22 @@ export default function ProgramField() {
                         Festivaldag med artister, UKElounge, stand-up og mye mer
                     </p>
                 </div>
+            </div> */}
+            <div className="program-card-wrapper">
+                <ProgramCard
+                    title="Pub til pub & Battle of the Bands"
+                    imagePath="/src/assets/billa_bar.jpg"
+                    date="Mandag 19. august"
+                >
+                    Bli med på pub til pub & Battle of the Bands.
+                </ProgramCard>
+                <ProgramCard
+                    title="Blinderntalenter"
+                    imagePath="/src/assets/blindern_talenter.jpg"
+                    date="Tirsdag 20. august"
+                >
+                    Bli med på intern dag med Blinderntalenter.
+                </ProgramCard>
             </div>
         </section>
     );
