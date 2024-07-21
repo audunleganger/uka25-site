6c6541bd279d8eea3b9a5a2ac50b69afb1dc3aff
Audun Leganger <audun.leganger@gmail.com>
2024-07-22 00:04:20 +0200

    Update tab icon

--- src/components/ProgramField.tsx
+++ src/components/ProgramField.tsx
@@ -9,22 +9,6 @@ export default function ProgramField() {
     return (
         <section className="program-wrapper">
             <h1 className="program-header">Program: MiniUKA på Blindern</h1>
-            {/*<div className="program-card-wrapper">
-                <div className="program-entry">
-                    <h3>Mandag 19. august</h3>
-                    <p>Pub til pub og Battle of the Bands</p>
-                </div>
-                <div className="program-entry">
-                    <h3>Tirsdag 20. august</h3>
-                    <p>Interndag med Blinderntalenter</p>
-                </div>
-                <div className="program-entry">
-                    <h3>Onsdag 21. august</h3>
-                    <p>
-                        Festivaldag med artister, UKElounge, stand-up og mye mer
-                    </p>
-                </div>
-            </div> */}
             <div className="program-card-wrapper">
                 <ProgramCard
                     title="Pub til pub & Battle of the Bands"
